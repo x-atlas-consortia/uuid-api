9f7f9c130e50a36b45e75eb8f72d9369799aac13
yuanzhou <yuanzhou19@gmail.com>
2020-08-17 21:30:55 -0400

    Added VERSION for docker image build

--- docker/docker-setup.sh
+++ docker/docker-setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Set the version environment variable for the docker build
+# Version number is from the VERSION file
+export UUID_API_VERSION=`cat VERSION`
+
+echo "UUID_API_VERSION: $UUID_API_VERSION"
+
 # Copy over the src folder
 cp -r ../src uuid-api/
 
