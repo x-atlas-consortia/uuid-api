65a0f59dfad98dc00fc15f8a10967bf6bb3a20b9
yuanzhou <yuanzhou19@gmail.com>
2020-08-18 23:13:05 -0400

    Added localhost build

--- docker/docker-setup.sh
+++ docker/docker-setup.sh
@@ -4,11 +4,9 @@
 # Version number is from the VERSION file
 # Also remove newlines and leading/trailing slashes if present in that VERSION file
 export UUID_API_VERSION=$(tr -d "\n\r" < ../VERSION | xargs)
-
 echo "UUID_API_VERSION: $UUID_API_VERSION"
 
 # Copy over the src folder
 cp -r ../src uuid-api/
-
 # Copy over the sql database
 cp -r ../sql/uuids-dev.sql hubmap-mysql/uuids-dev.sql
