f39cfa6082895569197e9f29df40d4769b79d0ff
Bill Shirey <shirey@pitt.edu>
2020-12-09 14:46:36 -0500

    new ddl for uuid schema update for generating submission ids

---
+++ reload_from_neo4j/reload_from_neo.py
@@ -0,0 +1,4 @@
+#Standalone script to reload, from scratch, the UUID relational database
+#This script reads all Nodes in a HuBMAP Neo4j database and stores all id
+#information in the UUID database
+
